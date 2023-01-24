d0432d658b26b39f1966a4fdae9beec3bccbc34d
Kamron <kamronpoosti@yahoo.com>
2023-01-23 18:29:28 -0800

    automobile form complete

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -3,16 +3,29 @@ import MainPage from './MainPage';
 import Nav from './Nav';
 import ManufacturersList from './ManufacturersList';
 import ManufacturersForm from './ManufacturersForm';
-import AutomobilesList from '.AutomobilesList'
 import React from 'react';
 import ModelList from './ModelList';
 import { useState, useEffect } from "react";
 import CreateModelForm from './CreateModelForm'
+import AutomobileForm from './AutomobileForm'
 
 
 function App(props) {
   const [manufacturers, setManufacturers] = useState([])
- 
+  const [models, setModels] = useState([])
+  const [automobiles, setAutomobiles] = useState([])
+
+  const getAutomobiles = async () => {
+    const url = 'http://localhost:8100/api/automobiles/'
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const automobiles = data.automobiles;
+      setAutomobiles(automobiles)
+    }
+  }
+
   const getManufacturers = async () => {
     const url = 'http://localhost:8100/api/manufacturers/'
     const response = await fetch(url);
@@ -23,8 +36,6 @@ function App(props) {
       setManufacturers(manufacturers)
     }
   }
-
-const [models, setModels] = useState([])
 
   const getModels = async () => {
     const response = await fetch('http://localhost:8100/api/models/');
@@ -37,6 +48,7 @@ const [models, setModels] = useState([])
 useEffect (() => {
   getModels();
   getManufacturers();
+  getAutomobiles();
 }, [])
 
   return (
@@ -47,7 +59,9 @@ useEffect (() => {
           <Route path="/" element={<MainPage />} />
           <Route path="manufacturers/" element={<ManufacturersList manufacturers={manufacturers} getManufacturers={getManufacturers} />} />
           <Route path="manufacturers/new" element={<ManufacturersForm getManufacturers={getManufacturers}/>} />
-          <Route path="automobiles/" element={<AutomobilesList automobiles={automobiles} getModels={getModels} />} />
+          <Route path="automobiles/">
+          <Route path="new" element={<AutomobileForm getAutomobiles={getAutomobiles}/>} />
+          </Route>
           <Route path="models/" element={<ModelList models={models} getModels={getModels}/>} />
           <Route path="models/">
           <Route path="new" element={<CreateModelForm getModels={getModels}/>} />
